e4874d8992da14acfcfac6b346c3994a8f759aae
Evan LeCompte <evan.lecompte@staples.com>
2015-03-17 13:08:07 -0400

    add images, fix data array

--- node/routes/index.js
+++ node/routes/index.js
@@ -6,17 +6,18 @@ var config = require('../../config');
 var url = require('url');
 
 var data = [
-    {id: 0, make: "hp", led: 255, preset: 0, img: "images/i0.jpg"},
-    {id: 1, make: "hp", led: 255, preset: 1, img: "images/i1.jpg"},
-    {id: 2, make: "hp", led: 255, preset: 2, img: "images/i2.jpg"},
-    {id: 3, make: "hp", led: 255, preset: 3, img: "images/i3.jpg"},
-    {id: 4, make: "hp", led: 255, preset: 4, img: "images/i4.jpg"},
-    {id: 5, make: "hp", led: 255, preset: 5, img: "images/i5.jpg"},
-    {id: 6, make: "hp", led: 255, preset: 6, img: "images/i6.jpg"},
-    {id: 7, make: "hp", led: 255, preset: 7, img: "images/i7.jpg"},
-    {id: 8, make: "hp", led: 255, preset: 8, img: "images/i8.jpg"},
-    {id: 9, make: "hp", led: 255, preset: 9, img: ""}, // Start position
-    {id: 10, make: "hp", led: 0, preset: 10, img: ""} // Off position
+    {id: 1, make: "hp", led: 255, img: "images/i0.jpg"},
+    {id: 2, make: "hp", led: 255, img: "images/i1.jpg"},
+    {id: 3, make: "hp", led: 255, img: "images/i2.jpg"},
+    {id: 4, make: "hp", led: 255, img: "images/i3.jpg"},
+    {id: 5, make: "hp", led: 255, img: "images/i4.jpg"},
+    {id: 6, make: "hp", led: 255, img: "images/i5.jpg"},
+    {id: 7, make: "hp", led: 255, img: "images/i6.jpg"},
+    {id: 8, make: "hp", led: 255, img: "images/i7.jpg"},
+    {id: 9, make: "hp", led: 255, img: "images/i8.jpg"},
+
+    {id: 16, make: "hp", led: 255, img: ""}, // Start position
+    {id: 17, make: "hp", led: 0, img: ""} // Off position
 ];
 
 /* GET home page. */
@@ -46,7 +47,7 @@ router.get('/inkpicker/:id', function(req, res, next) {
     request({
         uri: laserUrl,
         method: 'GET',
-        qs: {time: 50, led: item.led, preset: item.preset},
+        qs: {time: 50, led: item.led, preset: req.params.id},
         timeout: 500,
         time: true
     }, function(error, response, body) {
